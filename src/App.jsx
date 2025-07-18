a962f714f85de6fc54d0079af0de30e53af395a8
aceqasim <m.kaxim21@gmail.com>
2025-07-18 12:09:38 +0500

    completed the basic ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import StickyNote from "./components/StickyNote";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
+  const [notes, setNotes] = useState();
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <h1 className="m-3 text-white font-bold text-5xl italic"> Kvik Note</h1>
+      <div className="text-center">
+        <button className="bg-green-700 cursor-pointer hover:bg-green-800 text-white py-3 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-800 px-5 rounded-lg">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 4.5v15m7.5-7.5h-15"
+            />
+          </svg>
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="flex flex-wrap">
+        <StickyNote />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
